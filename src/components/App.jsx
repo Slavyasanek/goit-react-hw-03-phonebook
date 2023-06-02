14f073815803c5b70deb5384ff322a8a83617b8a
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-06-02 15:54:32 +0300

    add lifecycles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,26 @@ import ContactForm from "./ContactForm/ContactForm";
 import Filter from "./Filter/Filter";
 import ContactList from "./ContactList/ContactList";
 
+const CONTACTS_KEY = 'contacts';
 
 class App extends Component {
   state = {
     contacts: [],
     filter: ''
   }
+  componentDidMount() {
+    localStorage.getItem(CONTACTS_KEY) &&
+    this.setState({contacts: JSON.parse(localStorage.getItem(CONTACTS_KEY))})
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts.length > this.state.contacts.length) {
+      localStorage.setItem(CONTACTS_KEY, JSON.stringify(this.state.contacts))
+    } else if (prevState.contacts.length < this.state.contacts.length) {
+      localStorage.setItem(CONTACTS_KEY, JSON.stringify(this.state.contacts))
+    }
+  }
+
   addContact = data => {
     if (this.state.contacts.find(contact => contact.name.toLowerCase() === data.name.toLowerCase())) {
       alert(`${data.name} is already in contacts`);
